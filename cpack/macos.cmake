e55cc40ec8d0776abba1725d1714b3fb7b610c32
_yui <imbatman0xff@gmail.com>
2020-06-24 22:20:11 +0300

    CMake: Add a workaround to change bundle names back to X-Moto.app

--- cpack/macos.cmake
+++ cpack/macos.cmake
@@ -1,4 +1,4 @@
-set(CPACK_BUNDLE_NAME "XMoto")
+set(CPACK_BUNDLE_NAME "X-Moto")
 set(CPACK_BUNDLE_PLIST ${PROJECT_SOURCE_DIR}/macos/Info.plist)
 set(CPACK_BUNDLE_ICON ${PROJECT_SOURCE_DIR}/macos/xmoto.icns)
 
@@ -10,6 +10,12 @@ include(InstallRequiredSystemLibraries)
 
 if(BUILD_MACOS_BUNDLE)
   set_target_properties(xmoto PROPERTIES
+    # This changes the bundle name to "X-Moto.app", but it's
+    # actually a side effect of changing the executable name.
+    # As usual, it seems CMake doesn't provide even a half-reasonable way of doing it,
+    # so we have to put up with its garbage and have the executable named differently too.
+    OUTPUT_NAME "${CPACK_BUNDLE_NAME}"
+
     MACOSX_BUNDLE_INFO_PLIST
     "${PROJECT_SOURCE_DIR}/macos/Info.plist"
   )
@@ -20,6 +26,6 @@ if(BUILD_MACOS_BUNDLE)
 
   install(CODE "
     include(BundleUtilities)
-    fixup_bundle(\"\${CMAKE_INSTALL_PREFIX}/xmoto.app\"  \"\"  \"\")
+    fixup_bundle(\"\${CMAKE_INSTALL_PREFIX}/${CPACK_BUNDLE_NAME}.app\"  \"\"  \"\")
     " COMPONENT Runtime)
 endif()
